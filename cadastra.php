a65e999cc6728809889eb048dafcb07915d40aec
mdJaozin <jv29645@gmail.com>
2023-10-01 13:05:28 -0300

    starting

---
+++ cadastra.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h2>Cadastro de produtos</h2>
+
+    <div>
+        <form method="post">
+            <label for="nome" id="lblNome">Nome:</label>
+            
+            <input type="text" name="nome" id="nome">
+        </form>
+    </div>
+</body>
+</html>
